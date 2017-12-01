a673d1e66cc46b4fa37e67028aec9ffaa0c69d63
TheCell <simon_hischier@hotmail.com>
2017-12-01 09:36:56 +0100

    prepare Agents

---
+++ _02-Agents/02_01/gui.js
@@ -0,0 +1,34 @@
+let options = {
+	squareLength: 20,
+	squareWidth: 20,
+	rotationDegree: 45,
+	numberOfSquares: 30,
+	influneceWidthPercent: 40,
+	squareFillColor: [0, 0, 0], //RGB
+	backgroundColor: [40, 165, 180], //RGB
+};
+
+window.somethingChanged = true;
+let controllers = [];
+
+window.onload = function()
+{
+	window.gui = new dat.GUI();
+	window.gui.closed = true;
+
+	controllers.push(window.gui.add(options, 'squareLength').min(1).max(200).step(1));
+	controllers.push(window.gui.add(options, 'squareWidth').min(1).max(200).step(1));
+	controllers.push(window.gui.add(options, 'numberOfSquares').min(1).max(100).step(1));
+	controllers.push(window.gui.add(options, 'rotationDegree').min(0).max(360).step(1));
+	controllers.push(window.gui.add(options, 'influneceWidthPercent').min(0).max(100).step(1));
+	controllers.push(window.gui.addColor(options, 'squareFillColor'));
+	controllers.push(window.gui.addColor(options, 'backgroundColor'));
+
+	controllers.forEach( function(element, index, arr)
+	{
+		element.onChange( function (value)
+		{
+			window.somethingChanged = true;
+		});
+	});
+};
