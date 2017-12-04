f16fdd252cac193180c0122f2c81afcf050dbd87
TheCell <simon_hischier@hotmail.com>
2017-12-04 15:06:19 +0100

    added radio decay

---
+++ _02-Agents/02_02/gui.js
@@ -0,0 +1,111 @@
+let options = {
+	randomSeed: 2018,
+	numberOfAgents: 100,
+	moveSpeed: 2,
+	maxSpeed: 5,
+	agentFatness: 5,
+	turnRadius: Math.PI/32,
+	timeToLive: 100,
+	agentColor: [0, 0, 0], //RGB
+	backgroundColor: [255, 255, 255], //RGB
+	backgroundAlpha: 30, //RGB
+};
+
+window.somethingChanged = true;
+let controllers = [];
+
+window.onload = function()
+{
+	window.gui = new dat.GUI();
+	window.gui.closed = true;
+
+	let randomSeedOpt = window
+						.gui
+						.add(options, 'randomSeed')
+						.min(1)
+						.max(10000)
+						.step(1);
+	let numberOfAgents = window
+						.gui
+						.add(options, 'numberOfAgents')
+						.min(1)
+						.max(2000)
+						.step(1);
+	let agentFatness = window
+						.gui
+						.add(options, 'agentFatness')
+						.min(1)
+						.max(10)
+						.step(1);
+	let moveSpeed = window
+						.gui
+						.add(options, 'moveSpeed')
+						.min(1)
+						.max(5)
+						.step(0.1);
+	let backgroundAlpha = window
+						.gui
+						.add(options, 'backgroundAlpha')
+						.min(1)
+						.max(100)
+						.step(1);
+	let turnRadius = window
+						.gui
+						.add(options, 'turnRadius')
+						.min(0)
+						.max(PI/2)
+						.step(Math.PI/128);
+	let timeToLive = window
+						.gui
+						.add(options, 'timeToLive')
+						.min(1)
+						.max(1000)
+						.step(1);
+	controllers.push(randomSeedOpt);
+	controllers.push(numberOfAgents);
+	controllers.push(agentFatness);
+	controllers.push(turnRadius);
+	controllers.push(timeToLive);
+	controllers.push(moveSpeed);
+	controllers.push(backgroundAlpha);
+	controllers.push(window.gui.addColor(options, 'agentColor'));
+	controllers.push(window.gui.addColor(options, 'backgroundColor'));
+
+	controllers.forEach( function(element, index, arr)
+	{
+		element.onChange( function (value)
+		{
+			window.somethingChanged = true;
+		});
+	});
+
+	randomSeedOpt.onChange( resetAgents );
+	numberOfAgents.onChange( resetAgents );
+
+	function resetAgents ()
+	{
+		randomSeed(options.randomSeed);
+		agents = [];
+
+		for(let i = 0; i < options.numberOfAgents; i++)
+		{
+			let agent = new Agent(
+							random(0, windowWidth),
+							random(0, windowHeight),
+							15
+						);
+
+			agents.push(agent);
+		}
+	}
+
+	moveSpeed.onChange( function ()
+	{
+		let moveSpeed = this.object.moveSpeed;
+		agents.forEach( function (element, index, arr)
+		{
+			element.moveSpeed = moveSpeed;
+		})
+	});
+
+};
---
+++ _02-Agents/02_02/sketch.js
@@ -0,0 +1,393 @@
+"use strict";
+
+// Global var
+let mouseInputMode = 0;
+//let cycleInterval;
+let agents = [];
+
+function setup()
+{
+	// Canvas setup
+	canvas = createCanvas(windowWidth, windowHeight);
+	canvas.parent("p5Container");
+	// Detect screen density (retina)
+	var density = displayDensity();
+	pixelDensity(density);
+
+	randomSeed(options.randomSeed);
+
+	for(let i = 0; i < options.numberOfAgents; i++)
+	{
+		let agent = new Agent(
+						random(0, windowWidth),
+						random(0, windowHeight),
+						15
+					);
+
+		agents.push(agent);
+	}
+
+	/*
+	cycleInterval = window.setInterval( function ()
+	{
+		agents.forEach( function ( element, index, arr)
+		{
+			element.walkCycle();
+		});
+
+		window.somethingChanged = true;
+	}, 1000);
+	*/
+
+	background(options.backgroundColor, options.backgroundAlpha);
+}
+
+function draw()
+{
+	if (window.somethingChanged)
+	{
+		// draw
+		background(
+			options.backgroundColor[0],
+			options.backgroundColor[1],
+			options.backgroundColor[2],
+			options.backgroundAlpha);
+		stroke(color(options.agentColor));
+		strokeWeight(options.agentFatness);
+		fill(options.agentColor);
+		agents.forEach( function ( element, index, arr)
+		{
+			element.drawLocation();
+		});
+
+		let amountKilled = cleanDeadAgents(agents);
+		for (let i = 0; i < amountKilled; i++)
+		{
+			let agent = new Agent(
+				random(0, windowWidth),
+				random(0, windowHeight),
+				15);
+
+			agents.push(agent);
+		}
+
+		window.somethingChanged = false;
+	}
+
+	switch (mouseInputMode)
+	{
+		case 0:
+			window.somethingChanged = false;
+		break;
+		case 1:
+			window.somethingChanged = true;
+		break;
+		default:
+			window.somethingChanged = true;
+		break;
+	}
+
+	agents.forEach( function ( element, index, arr)
+	{
+		element.walkCycle();
+	});
+
+	window.somethingChanged = true;
+}
+
+function keyPressed()
+{
+	if (key == 's' || key == 'S') saveThumb(650, 350);
+	if (key == ' ')
+	{
+		mouseInputMode ++;
+		if (mouseInputMode > 1)
+		{
+			mouseInputMode = 0;
+		}
+	}
+	if (key == 'd' || key == 'D')
+	{
+	}
+	if (key == 'a' || key == 'A')
+	{
+	}
+}
+
+function cleanDeadAgents(arrayWithAgents)
+{
+	let killCount = 0;
+
+	arrayWithAgents.forEach( function(element, index, arr)
+	{
+		if (!element.agentAlive)
+		{
+			arr.splice(index, 1);
+			killCount ++;
+		}
+	});
+
+	return killCount;
+}
+
+class Agent
+{
+	constructor(pointX, pointY, lineLength)
+	{
+		this.location = createVector(pointX, pointY);
+		this.startAngle = random(0, TWO_PI);
+		this.lineLength = lineLength;
+		this.moveSpeed = options.moveSpeed;
+		this.points = [];
+		this.timeToLive = random(0, options.timeToLive);
+		this.agentAlive = true;
+
+		for (let i = 0; i < this.lineLength; i++)
+		{
+			let points = createVector(
+							this.location.x + cos(this.startAngle),
+							this.location.y + sin(this.startAngle)
+						);
+
+			this.points[i] = points;
+		}
+	}
+
+	drawLocation()
+	{
+		for (let i = 0; i < this.points.length; i++)
+		{
+			point(this.points[i].x, this.points[i].y);
+		}
+	}
+
+	walkCycle()
+	{
+		this.checkAndUpdateAngle();
+
+		if (this.timeToLive > 0)
+		{
+			// throw last array entry out
+			this.points.shift();
+			// up the movespeed the closer the mouse gets
+			let realSpeed = this.moveSpeed;
+			let distXFromMouse = this.location.x - mouseX; // -100 = left of mouse
+			let distYFromMouse = this.location.y - mouseY; // -100 = above mouse
+
+			/*
+			if ((distXFromMouse < 100 && distXFromMouse > -100)
+				&& (distYFromMouse < 100 && distYFromMouse > -100))
+			{
+				realSpeed = options.maxSpeed;
+				let headingBottom = false;
+				let headingLeft = false;
+
+				if (this.startAngle > 0 && this.startAngle < PI)
+				{
+					headingBottom = true;
+				}
+				if (this.startAngle > PI/2 && this.startAngle < 3*PI/2)
+				{
+					headingLeft = true;
+				}
+
+				if (distYFromMouse < 100 && distYFromMouse > 0)
+				{
+					if (distXFromMouse < 100 && distXFromMouse > 0)
+					{
+						// bottom right
+						if (headingBottom)
+						{
+							if (headingLeft)
+							{
+								this.startAngle -= options.turnRadius;
+							}
+							else
+							{
+								this.startAngle -= options.turnRadius;
+							}
+						}
+						else
+						{
+							// top left
+							if (headingLeft)
+							{
+								this.startAngle += options.turnRadius;
+							}
+							{
+								this.startAngle += options.turnRadius;
+							}
+						}
+					}
+					else
+					{
+						// bottom left
+						if (headingBottom)
+						{
+							if (headingLeft)
+							{
+								this.startAngle -= options.turnRadius;
+							}
+							else
+							{
+								this.startAngle += options.turnRadius;
+							}
+						}
+						else
+						{
+							if (headingLeft)
+							{
+								this.startAngle -= options.turnRadius;
+							}
+							{
+								this.startAngle -= options.turnRadius;
+							}
+						}
+					}
+
+				}
+				else
+				{
+					if (distXFromMouse < 100 && distXFromMouse > 0)
+					{
+						// top right
+						if (headingBottom)
+						{
+							if (headingLeft)
+							{
+								this.startAngle -= options.turnRadius;
+							}
+							else
+							{
+								this.startAngle -= options.turnRadius;
+							}
+						}
+						else
+						{
+							if (headingLeft)
+							{
+								this.startAngle += options.turnRadius;
+							}
+							else
+							{
+								this.startAngle -= options.turnRadius;
+							}
+						}
+					}
+					else
+					{
+						// top left
+						if (headingBottom)
+						{
+							if (headingLeft)
+							{
+								this.startAngle += options.turnRadius;
+							}
+							else
+							{
+								this.startAngle += options.turnRadius;
+							}
+						}
+						else
+						{
+							if (headingLeft)
+							{
+								this.startAngle -= options.turnRadius;
+							}
+							else
+							{
+								this.startAngle -= options.turnRadius;
+							}
+						}
+					}
+				}
+			}
+			*/
+
+
+			this.location.x = this.location.x + cos(this.startAngle) * realSpeed;
+			this.location.y = this.location.y + sin(this.startAngle) * realSpeed;
+			let differentLoc = Object.assign({}, this.location);
+			this.points[this.points.length] = differentLoc;
+
+			this.timeToLive --;
+		}
+		else
+		{
+			// throw last array entry out
+			this.points.shift();
+			if (this.points.length <= 0)
+			{
+				this.agentAlive = false;
+			}
+		}
+
+	}
+
+	checkAndUpdateAngle()
+	{
+		if (this.location.x > 0)
+		{
+			if (this.location.x < windowWidth)
+			{
+			}
+			else
+			{
+				// going out on the right
+				// pi random to the left
+				this.startAngle = random(PI/2, 3*PI/2);
+			}
+		}
+		else
+		{
+			// going out on the left
+			// pi random to the right
+			this.startAngle = (-PI/2) + random(0, PI);
+		}
+
+		if (this.location.y > 0)
+		{
+			if (this.location.y < windowHeight)
+			{
+			}
+			else
+			{
+				// going out on the bottom
+				this.startAngle = random(PI, TWO_PI);
+			}
+		}
+		else
+		{
+			// going out on the top
+			this.startAngle = random(0, PI);
+		}
+	}
+}
+
+// Tools
+
+// resize canvas when the window is resized
+function windowResized()
+{
+	resizeCanvas(windowWidth, windowHeight, false);
+	window.somethingChanged = true;
+}
+
+// Int conversion
+function toInt(value)
+{
+	return ~~value;
+}
+
+// Timestamp
+function timestamp()
+{
+	return Date.now();
+}
+
+// Thumb
+function saveThumb(w, h)
+{
+	let img = get( width/2-w/2, height/2-h/2, w, h);
+	save(img,'thumb.jpg');
+}
